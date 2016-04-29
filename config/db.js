84bbab029e9a537e2a2e8623e8a011f21a79e12a
dnahol <dalia.nahol@gmail.com>
2016-04-28 18:18:07 -0700

    just files

---
+++ config/db.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mysql = require('mysql');
+
+var db = mysql.createConnection( process.env.JAWSDB_URL || {
+  host    : 'localhost',
+  user    : 'root',
+  password: '978PoRsc',
+  database: 'bankdb',
+  debug   :  true
+
+});
+
+db.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected to the database...');
+});
+
+
+module.exports = db;
